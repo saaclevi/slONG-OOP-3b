1bd6e68d50c242e0f6d0e18207e832b0d4e118cc
Isaac Levi <isaac.levi@unesp.br>
2023-03-07 12:13:55 -0300

    Adicionar arquivos de projeto.

---
+++ slONG-OOP-3b/Animal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace slONG_OOP_3b
+{
+    internal class Animal
+    {
+        public int id { get; set; }
+        public string tipo { get; set; }
+        public string raca { get; set; }
+        public string cor { get; set; }
+        public int idade { get; set; }
+        public string ferimentos { get; set; }
+
+        //public void Info();
+    }
+}
---
+++ slONG-OOP-3b/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace slONG_OOP_3b
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal pet = new Animal();
+
+            Console.WriteLine("Digite os campos: ");
+            Console.WriteLine("ID: ");
+            pet.id = Convert.ToInt32( Console.ReadLine());
+            Console.WriteLine("Tipo: ");
+            pet.tipo = Console.ReadLine();
+            Console.WriteLine("Raça: ");
+            pet.raca = Console.ReadLine();
+            Console.WriteLine("Cor: ");
+            pet.cor = Console.ReadLine();
+            Console.WriteLine("Idade: ");
+            pet.idade = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("Ferimentos (S/N): ");
+                pet.ferimentos = Console.ReadLine();
+                Console.WriteLine(string.Compare(pet.ferimentos, "s");
+            } while (pet.ferimentos =! "S   ");
+
+
+            Console.WriteLine("")
+        }
+    }
+}
+ 
